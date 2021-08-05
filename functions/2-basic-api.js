0dd9a9c21c7717426eecbc268c22179ce923f1ec
Shirjeel Khalil Fahid <skf11987@gmail.com>
2021-08-05 18:14:12 +0100

    cors error fixed

--- functions/2-basic-api.js
+++ functions/2-basic-api.js
@@ -3,6 +3,9 @@ const items = require("../assets/data");
 exports.handler = async (event, context) => {
   console.log(event);
   return {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
     statusCode: 200,
     body: JSON.stringify(items),
   };
