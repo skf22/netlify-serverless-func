3cede17331e949f1e0eea465908d2703ca693cfd
Shirjeel Khalil Fahid <skf11987@gmail.com>
2021-08-07 17:15:45 +0100

    add survey

---
+++ functions/4-survey.js
@@ -0,0 +1,64 @@
+require("dotenv").config();
+const Airtable = require("airtable-node");
+
+const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY })
+  .base("appx4egGYhkalQoWU")
+  .table("survey");
+
+exports.handler = async (event, context, cb) => {
+  const method = event.httpMethod;
+  if (method === "GET") {
+    try {
+      const { records } = await airtable.list();
+      const survey = records.map((item) => {
+        const { id } = item;
+        const { room, votes } = item.fields;
+        return { id, room, votes };
+      });
+      return {
+        statusCode: 200,
+        body: JSON.stringify(survey),
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: "Server Error",
+      };
+    }
+  }
+
+  if (method === "PUT") {
+    try {
+      const { id, votes } = JSON.parse(event.body);
+      if (!id || !votes) {
+        return {
+          statusCode: 400,
+          body: "Please provide id and votes values",
+        };
+      }
+      const fields = { votes: Number(votes) + 1 };
+      const item = await airtable.update(id, { fields });
+      console.log(item);
+      if (item.error) {
+        return {
+          statusCode: 400,
+          body: JSON.stringify(item),
+        };
+      }
+      return {
+        statusCode: 200,
+        body: JSON.stringify(item),
+      };
+    } catch (error) {
+      return {
+        statusCode: 400,
+        body: "Please provide id and votes values.",
+      };
+    }
+  }
+
+  return {
+    statusCode: 405,
+    body: "Only GET and Put Requests Allowed.",
+  };
+};
